dc29f80eb1d1f36ac6e00b4181e7d23ee2462f68
MarcoAntonioPaixao <marco.antonio.paixao.neia>
2019-03-10 18:23:20 -0300

    Implementei metodo separador de tokens

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # Compilador Unbending
 
 Compilador para a linguagem de programação Unbending tendo sido ambos, linguagem e compilador, desenvolvidos durante o curso de Ciência da Computação.
+
+## Maneira indicada para compilar o código
+
+No diretório root do projeto use o seguinte comando no terminal:
+
+```bash
+  javac *.java
+```
+
+Isso irá compilar seu código e gravar os arquivos .class resultantes no diretório raiz do projeto.
+
+## Para rodar o código
+
+No diretório raiz do projeto usar o comando:
+
+```bash
+  java CompiladorUnbending <arquivo fonte>
+```
+
+Esse comando assume que o arquivo fonte esteja no diretorio raiz.
--- Token.java
+++ Token.java
@@ -6,4 +6,103 @@ class Token {
     this.conteudo = conteudo;
     this.tipoToken = tipoToken;
   }
+
+  public static Token identificaTipoToken(char conteudo) {
+    String tipo = "";
+
+    if (conteudo == '{')
+      tipo = "ABRE_CHAVE";
+    else if (conteudo == '}')
+      tipo = "FECHA_CHAVE";
+    else if (conteudo == '(')
+      tipo = "ABRE_PARENTESE";
+    else if (conteudo == ')')
+      tipo = "FECHA_PARENTESE";
+    else if (ehOpAritmetico(conteudo))
+      tipo = "OP_ARITMETICO";
+
+    return new Token(Character.toString(conteudo), tipo);
+  }
+
+  public static Token retornaIdentificador(String conteudoArquivo, int indiceAtual) {
+    String conteudoToken = "";
+
+    while (ehElementoValidoID(conteudoArquivo.charAt(indiceAtual))) {
+      conteudoToken += conteudoArquivo.charAt(indiceAtual);
+      indiceAtual++;
+    }
+
+    if (ehPalavraReservada(conteudoToken)) {
+      return new Token(conteudoToken, "RESERVADA");
+    } else {
+      return new Token(conteudoToken, "ID");
+    }
+  }
+
+  public static Token retornaNumero(String conteudoArquivo, int indiceAtual) {
+    String conteudoToken = "";
+    boolean flagNumeroReal = false;
+
+    while (ehElementoValidoNumero(conteudoArquivo.charAt(indiceAtual))) {
+      if (conteudoArquivo.charAt(indiceAtual) == '.')
+        flagNumeroReal = true;
+      conteudoToken += conteudoArquivo.charAt(indiceAtual);
+      indiceAtual++;
+    }
+
+    if (flagNumeroReal) {
+      return new Token(conteudoToken, "NUMERO_REAL");
+    } else {
+      return new Token(conteudoToken, "NUMERO_INTEIRO");
+    }
+
+  }
+
+  public static Token retornaOpLogico(String conteudoArquivo, int indiceAtual) {
+    String conteudoToken = "";
+
+    if (conteudoArquivo.charAt(indiceAtual) == '=' && conteudoArquivo.charAt(indiceAtual + 1) != '=')
+      return new Token("=", "ATRIBUICAO");
+    else {
+      if (conteudoArquivo.charAt(indiceAtual) == '!')
+        conteudoToken = "!";
+      if (conteudoArquivo.charAt(indiceAtual) == '!' && conteudoArquivo.charAt(indiceAtual + 1) == '=')
+        conteudoToken = "!=";
+      if (conteudoArquivo.charAt(indiceAtual) == '=' && conteudoArquivo.charAt(indiceAtual + 1) == '=')
+        conteudoToken = "==";
+      if (conteudoArquivo.charAt(indiceAtual) == '<' && conteudoArquivo.charAt(indiceAtual + 1) == '=')
+        conteudoToken = "<=";
+      if (conteudoArquivo.charAt(indiceAtual) == '>' && conteudoArquivo.charAt(indiceAtual + 1) == '=')
+        conteudoToken = ">=";
+      if (conteudoArquivo.charAt(indiceAtual) == '<' && conteudoArquivo.charAt(indiceAtual + 1) != '=')
+        conteudoToken = "<";
+      if (conteudoArquivo.charAt(indiceAtual) == '>' && conteudoArquivo.charAt(indiceAtual + 1) != '=')
+        conteudoToken = ">";
+      if (conteudoArquivo.charAt(indiceAtual) == '&' && conteudoArquivo.charAt(indiceAtual + 1) == '&')
+        conteudoToken = "&&";
+      if (conteudoArquivo.charAt(indiceAtual) == '|' && conteudoArquivo.charAt(indiceAtual + 1) == '|')
+        conteudoToken = "||";
+
+      return new Token(conteudoToken, "OP_LOGICO");
+    }
+
+  }
+
+  private static boolean ehElementoValidoID(char elemento) {
+    return (Character.isLetter(elemento) || elemento == '_');
+  }
+
+  private static boolean ehPalavraReservada(String palavra) {
+    return (palavra.equals("else") || palavra.equals("if") || palavra.equals("do") || palavra.equals("while")
+        || palavra.equals("read") || palavra.equals("write") || palavra.equals("int") || palavra.equals("float")
+        || palavra.equals("boolean"));
+  }
+
+  private static boolean ehElementoValidoNumero(char elemento) {
+    return (Character.isDigit(elemento) || elemento == '.');
+  }
+
+  private static boolean ehOpAritmetico(char elemento) {
+    return (elemento == '+' || elemento == '-' || elemento == '*' || elemento == '/');
+  }
 }
