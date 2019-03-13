b25a2f81a7128fa3dcf5aab16b340047a4a5599f
MarcoAntonioPaixao <marco.antonio.paixao.neia>
2019-03-13 10:18:17 -0300

    Corrigi bug retornando indice sendo menor em 1 do que o correto

--- AnalisadorLexico.java
+++ AnalisadorLexico.java
@@ -9,28 +9,28 @@ class AnalisadorLexico {
       if (!ehEspacoBranco(conteudoArquivo.charAt(i))) {
         if (ehLetra(conteudoArquivo.charAt(i))) {
 
-          System.out.println("Adicionei id ou palavra reservada");
+          // System.out.println("Adicionei id ou palavra reservada");
           Token idToken = Token.retornaIdentificador(conteudoArquivo, i);
-          i += idToken.conteudo.length();
+          i += idToken.conteudo.length() - 1;
           tokens.add(idToken);
 
         } else if (ehNumero(conteudoArquivo.charAt(i))) {
 
-          System.out.println("Adicionei numero");
+          // System.out.println("Adicionei numero");
           Token numeroToken = Token.retornaNumero(conteudoArquivo, i);
-          i += numeroToken.conteudo.length();
+          i += numeroToken.conteudo.length() - 1;
           tokens.add(numeroToken);
 
         } else if (ehOpLogico(conteudoArquivo.charAt(i))) {
 
-          System.out.println("Adicionei operador logico");
+          // System.out.println("Adicionei operador logico");
           Token opToken = Token.retornaOpLogico(conteudoArquivo, i);
           i += opToken.conteudo.length();
           tokens.add(opToken);
 
         } else if (ehTokenValido(conteudoArquivo.charAt(i))) {
 
-          System.out.println("Adicionei tipo de token extra");
+          // System.out.println("Adicionei tipo de token extra");
           Token token = Token.identificaTipoToken(conteudoArquivo.charAt(i));
           tokens.add(token);
 
